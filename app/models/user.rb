f624671ec2b937197387d701c6504642a10651e4
Rouzbeh-Berenji <berenji.ro@gmail.com>
2023-01-04 15:10:43 -0500

    user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, confirmation: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  def self.authenticate_with_credentials(email, password)
+    if user = self.where("lower(email) = ?", email.strip.downcase).first
+      user.authenticate(password)
+    end
+  end
 end
